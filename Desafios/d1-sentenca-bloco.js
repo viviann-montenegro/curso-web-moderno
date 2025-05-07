ab7ea75c8da9cc1e0a8713bde73add5e6e7606a4
Viviann Montenegro <viviann.dds.montenegro@gmail.com>
2025-05-07 18:18:43 -0300

    S3A21-22

---
+++ Desafios/d1-sentenca-bloco.js
@@ -0,0 +1,22 @@
+/*
+    Desafio #1 - Sentenças e Blocos
+    Imprimir, linha a linha:
+    * os números ímpares de 1 a 10, ordenando as sentenças dentro de um bloco
+    * os números pares de 1 a 10, ordenando as sentenças dentro de outro bloco
+*/ 
+
+{
+    console.log(1);
+    console.log(3);
+    console.log(5);
+    console.log(7);
+    console.log(9);
+}
+
+{
+    console.log(2);
+    console.log(4);
+    console.log(6);
+    console.log(8);
+    console.log(10);
+}
