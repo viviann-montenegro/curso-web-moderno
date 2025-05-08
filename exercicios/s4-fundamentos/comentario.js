1830df0f80086ab3ad0241eea7af1b983246c369
Viviann Montenegro <viviann.dds.montenegro@gmail.com>
2025-05-08 14:57:02 -0300

    S4A33-37

---
+++ exercicios/s4-fundamentos/comentario.js
@@ -0,0 +1,20 @@
+// Comentário de uma linha
+console.log('Linha 1')
+
+/*
+Comentário de 
+múltiplas linhas
+*/
+console.log('Linha 2')
+
+/*
+* Comentário de
+* múltiplas linhas (estilizado)
+*/
+console.log('Linha 3')
+
+/* Sobre comentários:
+Durante os estudos é importante comentar tudo que achar necessário para fixar o aprendizado.
+Em contextos de produção real, comentários sem necessidade devem ser dispensados,
+é preferível usar bons nomes autoexplicativos no código à comentar.
+*/
---
+++ exercicios/s4-fundamentos/organizacao.js
@@ -0,0 +1,5 @@
+console.log('Sntença de código')
+
+{
+    console.log('Sentença de código dentro de um bloco de código')
+}
